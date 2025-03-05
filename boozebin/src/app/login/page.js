03f24bb5d806252a74efd4157f7a1d17d25c4fde
Ben Sindberg <sindbergb@uwplatt.edu>
2025-03-04 18:27:28 -0600

    Register and Login UI base done

--- boozebin/src/app/login/page.js
+++ boozebin/src/app/login/page.js
@@ -3,21 +3,18 @@ import Link from 'next/link'
 import {Button} from "@heroui/react";
 import {Input} from "@heroui/react";
 
-export default function RegisterPage(){
+export default function LoginPage(){
     return (
         <div className="h-screen w-screen flex justify-center items-center bg-gradient-to-t from-black-800 to-purple-800">
-            <div className="shadow-xl px-8 pb-8 pt-12 rounded-xl space-y-12">
-            <h1 className="text-4xl font-semibold mb-12">Register</h1>
+            <div className="shadow-xl shadow-custom-shadow px-8 pb-8 pt-12 rounded-xl space-y-12">
+            <h1 className="text-4xl font-semibold mb-12">Log In</h1>
             
             <form className="space-y-12 w-[400px]">
-                <div className="grid w-full max-w-sm items-center gap-1.5">
-                <Input classNames={{input: ['outline-none', 'focus:outline-none'],}} label="Email" placeholder="Enter your email" type="email" />
+                <div key="faded" className="grid w-full max-w-sm items-center gap-1.5">
+                <Input classNames={{input: ['outline-none', 'focus:outline-none'],}} label="Email" variant="faded" placeholder="Enter your email" type="email" />
                 </div>
                 <div className="grid w-full max-w-sm items-center gap-1.5">
                 <Input classNames={{input: ['outline-none', 'focus:outline-none'],}} label="Password" placeholder="Enter Password" type="password"/>
-                </div>
-                <div className="grid w-full max-w-sm items-center gap-1.5">
-                <Input classNames={{input: ['outline-none', 'focus:outline-none'],}} label="Confirm Password" placeholder="Enter Password" type="password"/>
                 </div>
                 <div className="w-full">
                 <Button
@@ -25,11 +22,11 @@ export default function RegisterPage(){
                 radius="full"
                 fullWidth="true"
                 >
-                    Register
+                    Log In
                 </Button>
                 </div>
             </form>
-            <p>Have an account? <Link className='text-purple-300 hover:underline' href="/login">Log In</Link></p>
+            <p>Have an account? <Link className='text-purple-300 hover:underline' href="/register">Register</Link></p>
 
             </div>
         </div>   
--- boozebin/tailwind.config.js
+++ boozebin/tailwind.config.js
@@ -7,7 +7,11 @@ module.exports = {
     "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadowColor: {
+        'custom-shadow': 'rgba(204,153,255,1)',
+      },
+    },
   },
   darkMode: "class",
   plugins: [heroui()],
